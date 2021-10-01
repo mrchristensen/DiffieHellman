9c725dcb95cf29c265c2fe291939a16f26bc5620
Matt <mr.christensen@gmail.com>
2021-10-01 11:47:11 -0600

    Created an original mod_exp function (from fermat 312 project)

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 p = 12181492810988664495017491178372233835381077107491386374099654651874882048862073437146220579332510437000029696005029005194843775013033539704552333899762179  # openssl prime -safe -generate -bits 512
 a = 20891563773462123896204418683194062596038182229362433041917251470169172667990238136369636970581723525230222565705995177166942411535234742247842882232671835  # from /dev/urandom
-g = 5  # from spec:2wq:wq
+g = 5  # from spec
 
-g_b_mod_p = 308376848323412814814558943669072961568094875716095931067726756237444030568778979140516908036567120199097108245018498898501794560464940987551899675128711
+g_b_mod_p = 1516175301443637426131882779316154559936339848638096263390014710382066296168727316102600290100930665054572003397946894505779463189661166674146250661830725
+
+
+def mod_exp(x, y, N):
+    if y == 0:
+        return 1
+
+    z = mod_exp(x, y // 2, N)
+    if y % 2 == 0:  # If y is even
+        return (z ** 2) % N
+    else:  # If y is odd
+        return (x * (z ** 2)) % N
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    g_a_mod_p = pow(g, a, p)
-    print(g_a_mod_p)
+    print(f'Hard coded values:\np = {p}\na = {a}\ng = {g}\ng^b % p = {g_b_mod_p}\n')
 
-    g_ab_mod_p = pow(g_b_mod_p, a, p)
+    g_a_mod_p = mod_exp(g, a, p)
+    print(f'g^a % p = {g_a_mod_p}')
 
-    print(g_ab_mod_p)
-    print(g_ab_mod_p == 1251471439546230126277171520402169094304861686480199629304921740126112250060000619985676840081082263525023169765569226496998612580083679920212517971079432)
+    g_ab_mod_p = mod_exp(g_b_mod_p, a, p)
+
+    print(f'g^ab % p = {g_ab_mod_p}')
